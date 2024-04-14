779b67d514cc6dfdeeb8de25af37f9b2f0a95f76
Chust3r <kriptoortega@gmail.com>
2024-04-14 13:19:24 -0600

    [MIDDLEWARES] ADD REFRESH TOKEN

--- src/libs/jwt.ts
+++ src/libs/jwt.ts
@@ -1,13 +1,15 @@
 import { jwt as JWT } from '@elysiajs/jwt'
 
+const { EXP_JWT, EXP_REFRESH } = Bun.env
+
 export const jwt = JWT({
 	name: 'jwt',
 	secret: 'SECRET',
-	exp: '60m',
+	exp: EXP_JWT,
 })
 
 export const refresh = JWT({
 	name: 'refresh',
 	secret: 'SECRET',
-	exp: '90m',
+	exp: EXP_REFRESH,
 })
--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,25 +1,51 @@
 import { Elysia } from 'elysia'
-import { jwt } from '@libs/jwt'
+import { jwt, refresh } from '@libs/jwt'
 import { cookie } from '@schemas/request'
-
-//→ TODO: UPDATE REFRESH TOKEN
 
 export const authMiddleware = new Elysia()
 	.use(jwt)
+	.use(refresh)
 	.guard({ cookie })
-	.onBeforeHandle({ as: 'global' }, async ({ jwt, cookie }) => {
+	.onBeforeHandle({ as: 'global' }, async ({ jwt, refresh, cookie }) => {
 		try {
 			const token = cookie?.auth?.value?.token
 
-			if (!token) {
+			const refresh_token = cookie?.auth?.value?.refresh
+
+			if (!token || !refresh_token) {
 				return Response.json(undefined, { status: 401 })
 			}
 
 			const payload = await jwt.verify(token)
 
-			if (!payload) {
+			const payload_refresh = await refresh.verify(refresh_token)
+
+			if (payload) {
+				return
+			}
+
+			if (!payload_refresh) {
 				return Response.json(undefined, { status: 401 })
 			}
+
+			const new_token = await jwt.sign({
+				id: payload_refresh.id,
+				email: payload_refresh.email,
+			})
+
+			const new_refresh_token = await refresh.sign({
+				id: payload_refresh.id,
+				email: payload_refresh.email,
+			})
+
+			cookie.auth.set({
+				value: {
+					token: new_token,
+					refresh: new_refresh_token,
+				},
+				httpOnly: true,
+				sameSite: true,
+			})
 		} catch (e) {
 			console.log('[AUTH] MIDDLEWARE ERROR', e)
 			return Response.json(undefined, { status: 500 })
