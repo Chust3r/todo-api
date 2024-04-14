84b74634d9e3361a1ef3e086f046496befbe734b
Chust3r <kriptoortega@gmail.com>
2024-04-14 14:41:56 -0600

    [AUTH] LOGOUT

--- src/controllers/auth/logout/index.ts
+++ src/controllers/auth/logout/index.ts
@@ -1,7 +1,23 @@
-import Elysia from 'elysia'
+import { Elysia } from 'elysia'
+import { cookie } from '@schemas/request'
 
-export const logout = new Elysia().get('/logout', () => {
-	return Response.json({
-		message: 'AUTH LOGOUT',
-	})
-})
+const schema = {
+	cookie,
+}
+
+// TODO: IMPLEMENT LOGOUT IN DB
+
+export const logout = new Elysia().get(
+	'/logout',
+	async ({ cookie }) => {
+		try {
+			cookie.auth.remove()
+
+			return Response.json(undefined, { status: 200 })
+		} catch (e) {
+			console.log('[AUTH] LOGOUT ERROR')
+			return Response.json(undefined, { status: 500 })
+		}
+	},
+	schema
+)
