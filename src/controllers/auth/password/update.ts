b7e92ea67293d2039ce6c3ce14f98550426dc39e
Chust3r <kriptoortega@gmail.com>
2024-04-19 20:21:20 -0600

    [AUTH] PASSWORD UPDATE

--- src/controllers/auth/password/update.ts
+++ src/controllers/auth/password/update.ts
@@ -1,11 +1,64 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
 import { cookie } from '@schemas/request'
 import { authMiddleware } from '@middlewares/auth'
 
-export const update = new Elysia().post('/update', async () => {
-	try {
-		return Response.json(undefined, { status: 200 })
-	} catch (e) {
-		console.log('[AUTH] PASSWORD UPDATE ERROR')
-	}
+// → DATABASE
+
+import { db, users } from '@db'
+import { eq } from 'drizzle-orm'
+
+const body = t.Object({
+	password: t.String(),
+	newPassword: t.String(),
 })
+
+const schema = {
+	cookie,
+	body,
+}
+
+export const update = new Elysia().use(authMiddleware).post(
+	'/update',
+	async ({ cookie, jwt, body }) => {
+		try {
+			const token = cookie?.auth?.value?.token
+
+			const payload = await jwt.verify(token)
+
+			if (!payload) {
+				return Response.json(undefined, { status: 401 })
+			}
+
+			const user = await db.query.users.findFirst({
+				where: (users) => eq(users.id, payload?.id as string),
+			})
+
+			if (!user) {
+				return Response.json(undefined, { status: 401 })
+			}
+
+			const isValid = await Bun.password.verify(body.password, user.password)
+
+			if (!isValid) {
+				return Response.json(undefined, { status: 401 })
+			}
+
+			const password = await Bun.password.hash(body.newPassword, {
+				algorithm: 'argon2i',
+				memoryCost: 1024,
+				timeCost: 3,
+			})
+
+			await db
+				.update(users)
+				.set({ password })
+				.where(eq(users.id, payload?.id as string))
+
+			return Response.json(undefined, { status: 200 })
+		} catch (e) {
+			console.log('[AUTH] PASSWORD UPDATE ERROR')
+			return Response.json(undefined, { status: 500 })
+		}
+	},
+	schema
+)
